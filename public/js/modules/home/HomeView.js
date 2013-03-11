579a0aab35652d7c67cb1fa00d06958338d928d9
Christina <blurei@gmail.com>
2013-03-11 10:22:00 -0400

    put in an onclose get rid fo scroll bar

--- public/js/modules/home/HomeView.js
+++ public/js/modules/home/HomeView.js
@@ -1,12 +1,18 @@
 define(function(require) {
-    "use strict";
+	"use strict";
 
-    var BaseView = require("modules/base/BaseView");
-    var homeTemplate = require("text!templates/home/home.html");
+	var BaseView = require("modules/base/BaseView");
+	var homeTemplate = require("text!templates/home/home.html");
 
-    var HomeView = BaseView.extend({
-        template: homeTemplate
-    });
+	var HomeView = BaseView.extend({
+		
+		template : homeTemplate,
+		
+		onClose: function() {
+			//unbind events and close subviews here
+		}
+		
+	});
 
-    return HomeView;
-});
+	return HomeView;
+}); 
